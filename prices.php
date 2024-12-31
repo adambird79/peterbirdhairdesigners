eaf19f729281de30238015707067e4b9b442a064
Adam Bird <adam.bird79@gmail.com>
2024-12-31 08:46:17 +0000

    Add files via upload

---
+++ prices.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Peter Bird Hair Design</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26276318-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<link rel="stylesheet" href="css/standard.css" type="text/css" media="screen" />
+
+	
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<div id="wrapper"> 
+<div class="jobs">
+<a href="jobs.htm"><img src="Images/JO.png" alt="Now Recruiting Staff" width="150" height="150"></a>
+</div>
+  <table width="1000" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td colspan=6><img src="Images/spire.jpg"></td>
+  </tr>
+  <tr>
+<td colspan=5 background="Images/PeterBirdBanner.jpg">
+		<table border="0" cellspacing="0" cellpadding="0">
+          <tr> 
+            <td width="425"><img src="Images/transparent.gif" height="28" width="425" border=0></td>
+            <td width="100"><img src="Images/transparent.gif" height="28" width="100" border=0></td>
+            <td width="25"><img src="Images/transparent.gif" height="28" width="25" border=0></td>
+            <td width="150"><img src="Images/transparent.gif" height="28" width="150" border=0></td>
+            
+          </tr>
+          <tr> 
+            <td><img src="Images/transparent.gif" height="100" width="425" border=0></td>
+            <td class="shops"> 10 Corporation Street<br/>
+              Rotherham<br/>
+              S60 1NG<br/> <br/>
+              01709 374048</td>
+            <td><img src="Images/transparent.gif" height="28" width="20" border=0></td>
+		  <tr>
+		  	<td colspan=2>
+				<table border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td class="mainlink"><a href="index.htm">Home</a></td>
+                  <td class="mainlink"><a href="prices.php">Prices</td>
+                  <td class="mainlink"><a href="gallery.htm">Gallery</a></td>
+                  <td class="mainlink"><a href="Middle_Aged.htm">Middle&nbsp;Aged?</td></td>
+                   <td class="mainlink"><a href="jobs.htm">Jobs</a></td>
+				  <td class="mainlink"></td>
+                </tr>
+              </table>
+</td>
+            <td><img src="Images/transparent.gif" height="28" width="25" border=0></td>
+            <td><img src="Images/transparent.gif" height="28" width="150" border=0></td>
+		  </tr>
+		</table>
+    </td>
+  </tr>
+  <tr>
+    <td colspan=3 background="Images/mainChurchFade.jpg" valign="top">
+		<table width="606" border="0" cellspacing="0" cellpadding="0">
+          <tr> 
+            <td><img src="Images/transparent.gif" height="70" width="17" border=0></td>
+            <td><img src="Images/transparent.gif" height="70" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="70" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="70" width="37" border=0></td>
+            <td><img src="Images/transparent.gif" height="70" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="70" width="138" border=0></td>
+          </tr>
+          <tr> 
+            <td>&nbsp;</td>
+            <td align="center" colspan="6" class="new_hair_head"><div align="left">Prices</div></td>
+          </tr>
+          <tr> 
+            <td>&nbsp;</td>
+            <td align="center" colspan="6" class="new_hair_head">
+				<table border="0">
+					<tr><td><h3>Styling Menu</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr>
+					<tr><td>Dry Trim</td><td>&#163;10</td><td>&#163;10</td><td>&#163;10</td>
+					<tr><td>Cut and Finish Short</td><td>&#163;20</td><td>&#163;24</td><td>&#163;27</td>
+					<tr><td>Cut and Finish Medium</td><td>&#163;21</td><td>&#163;25</td><td>&#163;28</td>
+					<tr><td>Cut and Finish Long</td><td>&#163;22</td><td>&#163;26</td><td>&#163;29</td>
+					<tr><td>New Style and Finish</td><td>&#163;23</td><td>&#163;27</td><td>&#163;30</td>
+					<tr><td>Wet Spray Cut</td><td>&#163;12</td><td>&#163;15</td><td>&#163;17</td>
+					<tr><td>Put Up Styles</td><td>&#163;16</td><td>&#163;17</td><td>&#163;20</td>
+					<tr><td>Blow Dry Finish</td><td>&#163;10</td><td>&#163;11</td><td>&#163;12</td></tr>
+					<tr><td><h3>Colour Menu</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Full Head Colout</td><td>&#163;25</td><td>&#163;25</td><td>&#163;27</td><tr><td>Regrowth</td><td>&#163;23</td><td>&#163;23</td><td>&#163;23</td></tr><tr><td><h3>Foils</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Full Head</td><td>&#163;29</td><td>&#163;30</td><td>&#163;32</td><tr><td>Half Head</td><td>&#163;21</td><td>&#163;22</td><td>&#163;25</td><tr><td>Parting</td><td>&#163;14</td><td>&#163;15</td><td>&#163;16</td><tr><td>Singles</td><td>&#163;3</td><td>&#163;4</td><td>&#163;5</td><tr><td>Extra Colours</td><td>&#163;5</td><td>&#163;7</td><td>&#163;7</td><tr><td>Pre-Lighteners</td><td>&#163;28</td><td>&#163;29</td><td>&#163;30</td><tr><td>Pre-Pigment</td><td>&#163;12</td><td>&#163;14</td><td>&#163;16</td><tr><td>Cap Highlights</td><td>&#163;22</td><td>&#163;23</td><td>&#163;24</td></tr><tr><td><h3>Spatula Highlights</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Full Head</td><td>&#163;23</td><td>&#163;25</td><td>&#163;27</td><tr><td>Half Head</td><td>&#163;18</td><td>&#163;20</td><td>&#163;22</td><tr><td>Parting</td><td>&#163;14</td><td>&#163;16</td><td>&#163;18</td><tr><td>Extra Colours</td><td>&#163;7</td><td>&#163;7</td><td>&#163;7</td><tr><td>Semi-Permanent</td><td>&#163;22</td><td>&#163;23</td><td>&#163;25</td><tr><td>Full Colour Change</td><td colspan=3>Estimates After Consultation</td><tr><td>Colour Correction</td><td colspan=3>Estimates After Consultation</td><tr><td>Colour Condition</td><td>&#163;9</td><td>&#163;10</td><td>&#163;12</td></tr><tr><td><h3>Styling Support Menu</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Short Perm</td><td>&#163;30</td><td>&#163;32</td><td>&#163;39</td><tr><td>Spiral Perm</td><td>&#163;50</td><td>&#163;55</td><td>&#163;60</td><tr><td>Half Head</td><td>&#163;20</td><td>&#163;22</td><td>&#163;24</td><tr><td>Relaxer Straightener</td><td>&#163;22</td><td>&#163;25</td><td>&#163;27</td></tr><tr><td><h3>Conditioning Treatments</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td></td><td colspan=3>&#163;4</td></tr><tr><td><h3>SPECIALS OAPS 25 Per Cent Discounts</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>OAP Trims (Gents)</td><td>&#163;5</td><td>&#163;5</td><td>&#163;5</td><tr><td>OAP Trims (Ladies)</td><td>&#163;8</td><td>&#163;8</td><td>&#163;10</td><tr><td>Finishes</td><td>&#163;7.5</td><td>&#163;8.25</td><td>&#163;9</td><tr><td>OAP Perms (Standard)</td><td>&#163;22.5</td><td>&#163;24</td><td>&#163;29.25</td><tr><td>Shampoo and Sets</td><td>&#163;8</td><td>&#163;8</td><td>&#163;8</td></tr><tr><td><h3>SPECIALS KIDS</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Child Trim (Under 12)</td><td>&#163;7</td><td>&#163;7</td><td>&#163;7</td><tr><td>Cut and Finishes</td><td colspan=3>25 Per Cent Discounts</td><tr><td>Child Finishes</td><td colspan=3>25 Per Cent Discounts</td><tr><td>Colour</td><td colspan=3>Same as Normal</td></tr><tr><td><h3>Gents</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Trims</td><td>&#163;8</td><td>&#163;8</td><td>&#163;8</td><tr><td>New Style</td><td>&#163;10</td><td>&#163;10</td><td>&#163;12</td><tr><td>Beard Trim</td><td>&#163;3</td><td>&#163;4</td><td>&#163;5</td></tr><tr><td><h3>Extensions</h3></td><td valign='bottom'><strong>Stylist</strong></td><td valign='bottom'><strong>Senior<br/>Stylist</strong></td><td valign='bottom'><strong>Peter<br/>Bird</strong></td></tr><tr><td>Full Head</td><td colspan=3>Estimates Given on Consultation</td><tr><td>Half Head</td><td colspan=3>Estimates Given on Consultation</td><tr><td>Pieces</td><td colspan=3>Estimates Given on Consultation</td></tr> 
+				</table>
+			</td>
+          </tr>
+          
+          <tr> 
+            <td><img src="Images/transparent.gif" height="20" width="17" border=0></td>
+            <td><img src="Images/transparent.gif" height="20" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="20" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="20" width="37" border=0></td>
+            <td><img src="Images/transparent.gif" height="20" width="138" border=0></td>
+            <td><img src="Images/transparent.gif" height="20" width="138" border=0></td>
+          </tr>
+        </table>
+	</td>
+    <td colspan=2 valign="top"><img src="Images/girlFade.jpg" width="394" height="483" border=0></td>
+  </tr>
+  <tr>
+  	<td colspan=6 class="footer" align="left">
+		<a href="http://www.peterbirdhairdesigners.co.uk">http://www.peterbirdhairdesigners.co.uk</a>&nbsp;&nbsp;|&nbsp;&nbsp;10 Corporation Street, Rotherham. S60 1NG. 01709 374048
+	</td>
+  </tr>
+
+</table>
+
+</div>
+
+</body>
+</html>
